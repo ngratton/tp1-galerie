02a074ac9d904aac9e9ba6fa446f8511000d4e71
Nicholas Gratton <nichgratton@gmail.com>
2019-08-12 08:57:30 -0400

    Ajout de la fonction clic.

--- src/Galerie.js
+++ src/Galerie.js
@@ -7,8 +7,73 @@ export default class Galerie {
 	 * Méthode principale. Sera typiquement appelée après le chargement de la page.
 	 */
 	static main() {
-		console.log("Je suis prêt");
-		document.getElementById("app").innerHTML = "La page est chargée";
+		this.app = document.getElementById("app");
+		this.ajouterClic();
+	}
+	static ajouterClic() {
+		var liens = Array.from(this.app.querySelectorAll("#galerie a"));
+		for (let i = 0; i < liens.length; i = i + 1) {
+			var leLien = liens[i];
+			leLien.addEventListener("click", e => {
+				e.preventDefault(); // Enlève la fonction définie dans le HTML; dans ce cas-ci, l'ouverture de l'image dans un nouvel onglet.
+				var position = liens.indexOf(e.currentTarget);
+				position = position + 1;
+				var nextImg = liens[position - 1];
+				var prevImg = liens[position + 1];
+				document.body.appendChild(this.retournerBackdrop(e.currentTarget, prevImg, nextImg));
+			});
+		}
+	}
+	static retournerBackdrop(target, prevImg, nextImg) {
+		var contenu = target.querySelector("img");
+		var imgAnimal = contenu.getAttribute("src");
+		var imgFigure = contenu.getAttribute("alt");
+
+		var backdrop = document.createElement("div");
+		backdrop.setAttribute("id", "backdrop");
+
+		var close = document.createElement("span");
+		close.classList.add("close");
+		close.innerHTML = "&#x2716;";
+		backdrop.appendChild(close);
+
+		var precedent = document.createElement("span");
+		precedent.classList.add("precedent");
+		precedent.innerHTML = "&#x276e;";
+		backdrop.appendChild(precedent);
+
+		var figure = document.createElement("figure");
+		figure.classList.add("diapo");
+		figure.style.transition = "all 0.5s ease 0s";
+		figure.style.transform = "scale(1)"
+		backdrop.appendChild(figure);
+
+		var imgtag = document.createElement("img");
+		imgtag.src = imgAnimal;
+		imgtag.setAttribute("alt", imgFigure);
+		figure.appendChild(imgtag);
+
+		var figcaption = document.createElement("figcaption");
+		figcaption.innerHTML = imgFigure;
+		figure.appendChild(figcaption);
+
+		var suivant = document.createElement("span");
+		suivant.classList.add("suivant");
+		suivant.innerHTML = "&#x276f;";
+		backdrop.appendChild(suivant);;
+
+		this.fermerBackdrop();
+
+		return backdrop;
+	}
+	// Fonction qui permet de "tuer" le DIV#backdrop avec la touche "ESC" -- devrait utiliser le click.
+	static fermerBackdrop() {
+		addEventListener("click", e => {
+			if(e.target === document.querySelector("div#backdrop") || e.target === document.querySelector("span.close")) {
+				var killBackdrop = document.body.querySelector("div#backdrop");
+				killBackdrop.parentNode.removeChild(killBackdrop);
+			}
+		});
 	}
 	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
